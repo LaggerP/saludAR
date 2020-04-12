5579439db4aea79761c1197bad32e2039beee340
Pablo <contacto@laggerp.me>
2020-04-12 20:44:39 -0300

    Deleted arg flag

--- saludar/src/components/Landing.jsx
+++ saludar/src/components/Landing.jsx
@@ -11,7 +11,7 @@ class Landing extends Component {
                     <div className="Landing-header-title">
                         <h1 className="animated bounceIn"><span>👏Sal</span>
                             <span>ud</span>
-                            <span>AR🇦🇷</span></h1>
+                            <span>AR</span></h1>
 
                         <p className="animated fadeIn">Para aquellas personas que salen, se arriesgan y nos cuidan.</p>
                         <p className="animated fadeIn">Les decimos GRACIAS<span className="heart">❤️</span></p>
