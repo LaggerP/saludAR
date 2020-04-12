17f1f94c8235a9f7a9c0896269fedb556c142466
LaggerP <pablo_lagger@hotmail.com>
2020-04-12 19:30:11 -0300

    added animations and solved responsive error

--- saludar/src/components/Landing.jsx
+++ saludar/src/components/Landing.jsx
@@ -1,27 +1,28 @@
 import React, {Component} from 'react';
 import './Landing.scss';
+import '../animate.css'
 import MensajeForm from "./MensajeForm/MensajeForm";
 
 class Landing extends Component {
     render() {
         return (
             <div>
-                <div className="Landing-header">
+                <div className="Landing-header ">
                     <div className="Landing-header-title">
-                        <h1><span>👏Sal</span>
+                        <h1 className="animated bounceIn"><span>👏Sal</span>
                             <span>ud</span>
                             <span>AR🇦🇷</span></h1>
 
-                        <p>Para aquellas personas que salen, se arriesgan y nos cuidan.</p>
-                        <p>Les decimos <span>GRACIAS❤️</span></p>
+                        <p className="animated fadeIn">Para aquellas personas que salen, se arriesgan y nos cuidan.</p>
+                        <p className="animated fadeIn">Les decimos GRACIAS<span className="heart">❤️</span></p>
                     </div>
 
                 </div>
-                <div className="Landing-header-body">
+                <div className="Landing-header-body animated bounceInUp">
                     <MensajeForm></MensajeForm>
                 </div>
                 <div className="Landing-header-footer">
-                     <span>Realizado con ❤️ por <a href="https://github.com/LaggerP">Pablo Lagger</a></span>
+                    <span>Realizado con ❤️ por <a href="https://github.com/LaggerP">Pablo Lagger</a></span>
                 </div>
             </div>
         );
